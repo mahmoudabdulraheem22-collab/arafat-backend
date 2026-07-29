5e6d0ec8b1ee8e443414afd277cb2b7fbb53fa03
mahmoudabdulraheem22-collab <mahmoudabdulraheem22@gmail.com>
2026-07-30 00:36:19 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,17 @@
-import { fileURLToPath, URL } from 'node:url';
-import { defineConfig } from 'vite';
-import react from '@vitejs.plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('.', import.meta.url)),
+      '@': path.resolve(process.cwd(), '.'),
     },
   },
   server: {
     host: '0.0.0.0',
     allowedHosts: true,
-    hmr: process.env?.DISABLE_HMR !== 'true',
-    watch: process.env?.DISABLE_HMR === 'true' ? null : {},
   },
 });
